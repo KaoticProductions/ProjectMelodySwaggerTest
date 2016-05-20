5e87a842e93a0ad7920482a9a5d8b10a161c00ef
aklein <aaronrklein@gmail.com>
2016-05-19 23:46:48 -0700

    Added /Account and /Accounts to the REST API

--- melody-swagger-test/api/controllers/characters.js
+++ melody-swagger-test/api/controllers/characters.js
@@ -1,30 +1,19 @@
-var util 				    = require('util');
-var mysql       		= require("mysql");
+var sql_helper = require('../helpers/sql_helper');
 
 module.exports = {
   characters: characters
 };
 
-const PORT = 2021;
-
 function characters(req, res) {
   var query = "SELECT * FROM Characters";
-
-  var connection = mysql.createConnection({
-    host            	: process.env.MELODY_DB_HOST,
-    user            	: process.env.MELODY_DB_USER,
-    port            	: process.env.MELODY_DB_PORT,
-    password   	      : process.env.MELODY_DB_PASSWORD,
-    database     	    : process.env.MELODY_DB_DATABASE
-  });
-  connection.connect();
-  connection.query(query,function(err,rows){
+  sql_helper.GetPool().query(query,function(err, rows){
     if(err) {
-        res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        console.log(err)
+        res.json({"error" : true, "message" : "Error executing MySQL query"});
     }
     else {
-        res.json({"Error" : false, "Message" : "Success", "Characters" : JSON.stringify(rows)});
+        console.log(rows);
+        res.json({"error" : false, "message" : "Success", "characters" : JSON.stringify(rows)});
     }
   });
-  connection.end();
 }
